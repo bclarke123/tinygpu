e962850755e2fbdfccf7ccf4595ea568ecd0bbf8
Ben Clarke <ben@arrayofstars.com>
2025-05-04 17:31:47 -0400

    Start trying to use the API

--- test/index.js
+++ test/index.js
@@ -1,32 +1,3 @@
-const shader = /* wgsl */`
-  struct VSOut {
-    @builtin(position) position: vec4<f32>,
-    @location(0) uv: vec2<f32>,
-  };
-
-  @vertex
-  fn vs_main(@builtin(vertex_index) vertex_index: u32) -> VSOut {
-    let pos = array<vec2<f32>, 6>(
-      vec2<f32>(-1.0, -1.0), vec2<f32>( 1.0, -1.0), vec2<f32>(-1.0,  1.0),
-      vec2<f32>(-1.0,  1.0), vec2<f32>( 1.0, -1.0), vec2<f32>( 1.0,  1.0),
-    );
-      let uvs = array<vec2<f32>, 6>(
-      vec2<f32>(0.0, 1.0), vec2<f32>(1.0, 1.0), vec2<f32>(0.0, 0.0),
-      vec2<f32>(0.0, 0.0), vec2<f32>(1.0, 1.0), vec2<f32>(1.0, 0.0),
-    );
-
-    var vs_out: VSOut;
-    vs_out.position = vec4<f32>(pos[vertex_index], 0.0, 1.0);
-    vs_out.uv = uvs[vertex_index]; // Pass UVs directly
-    return vs_out;
-  }
-
-  @fragment
-  fn fs_main(vs_out: VSOut) -> @location(0) vec4<f32> {
-    return vec4(vs_out.uv.x, vs_out.uv.y, 0.0, 1.0);
-  }
-  `;
-
 document.addEventListener("DOMContentLoaded", async () => {
     const canvas = document.getElementById("canvas");
     const renderer = new tinygpu.Renderer({ canvas });
@@ -34,11 +5,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     console.log(renderer.device);
 
-    const shaderModule = renderer.compileShader(shader);
-    console.log(shaderModule);
+    const mat = renderer.materialFactory.createBasicMaterial({ color: new tinygpu.Color(1, 0, 0, 1) });
+    const geo = renderer.geometryFactory.createBigTriangle();
 
-    const pipeline = renderer.createPipeline(shaderModule);
-    console.log(pipeline);
+    const mesh = renderer.createMesh(geo, mat);
 
-    renderer.encodeCommands(pipeline);
+    console.log(mesh);
+
+    // renderer.encodeCommands(pipeline);
 });
