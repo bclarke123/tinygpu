ca93b59e0c5c7fa079db5f6383fd0b2b82a70661
Ben Clarke <ben@arrayofstars.com>
2025-05-04 17:29:51 -0400

    Exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 export { Renderer, RendererOptions } from "./renderer";
+export { MaterialFactory } from "./materials/material-factory";
+export { GeometryFactory } from "./geometry/geometry-factory";
+export { BigTriangle } from "./geometry/big-triangle";
+export { BasicMaterial, BasicMaterialOptions } from "./materials/basic-material";
+export { Color } from "./color";
 
 export class PerspectiveCamera {
   constructor() {
