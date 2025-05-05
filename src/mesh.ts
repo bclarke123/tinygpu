42e18815c1087cd630b5d68d439a8e6911c6113c
Ben Clarke <ben@arrayofstars.com>
2025-05-05 19:28:56 -0400

    Mesh extends Transform, fix up bind group

--- src/mesh.ts
+++ src/mesh.ts
@@ -1,12 +1,11 @@
-import { mat4, Mat4 } from "wgpu-matrix";
 import { Geometry } from "./geometry/geometry";
 import { Material } from "./materials/material";
 import { packUniforms, uploadUniformBuffer } from "./uniform-utils";
+import { Transform } from "./transform";
 
-export class Mesh {
+export class Mesh extends Transform {
     material: Material;
     geometry: Geometry;
-    matrix: Mat4;
     uniformArr: ArrayBuffer;
 
     _device: GPUDevice;
@@ -15,10 +14,10 @@ export class Mesh {
     _bindGroup?: GPUBindGroup;
 
     constructor(device: GPUDevice, mat: Material, geo: Geometry) {
+        super();
         this._device = device;
         this.material = mat;
         this.geometry = geo;
-        this.matrix = mat4.identity();
     }
 
     get cacheKey(): string {
@@ -27,7 +26,7 @@ export class Mesh {
 
     get uniformBuffer(): ArrayBuffer {
         const uniforms = [
-            { name: "model", value: this.matrix },
+            { name: "model", value: this.worldMatrix },
         ];
 
         this.uniformArr = packUniforms(uniforms);
@@ -46,41 +45,47 @@ export class Mesh {
     }
 
     get bindGroupLayout(): GPUBindGroupLayout {
-        const layoutDescriptor: GPUBindGroupLayoutDescriptor = {
-            label: "Mesh Uniforms BindGroup Layout",
-            entries: [
-                {
-                    binding: 0,
-                    visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
-                    buffer: {
-                        type: 'uniform',
-                        hasDynamicOffset: false,
-                        minBindingSize: 0
+        if (!this._bindGroupLayout) {
+            const layoutDescriptor: GPUBindGroupLayoutDescriptor = {
+                label: "Mesh Uniforms BindGroup Layout",
+                entries: [
+                    {
+                        binding: 0,
+                        visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+                        buffer: {
+                            type: 'uniform',
+                            hasDynamicOffset: false,
+                            minBindingSize: 0
+                        }
                     }
-                }
-            ]
-        };
+                ]
+            };
 
-        const meshBindGroupLayout = this._device.createBindGroupLayout(layoutDescriptor);
+            this._bindGroupLayout = this._device.createBindGroupLayout(layoutDescriptor);
 
-        console.log("Created Mesh BindGroupLayout:", meshBindGroupLayout);
-        return meshBindGroupLayout;
+            console.log("Created Mesh BindGroupLayout:", this._bindGroupLayout);
+        }
+        return this._bindGroupLayout;
     }
 
     get bindGroup(): GPUBindGroup {
         const uniforms = this.uploadUniforms();
 
-        const layoutDescriptor: GPUBindGroupDescriptor = {
-            label: "Mesh BindGroup",
-            layout: this.bindGroupLayout,
-            entries: [
-                {
-                    binding: 0,
-                    resource: { buffer: uniforms },
-                }
-            ]
+        if (!this._bindGroup) {
+            const layoutDescriptor: GPUBindGroupDescriptor = {
+                label: "Mesh BindGroup",
+                layout: this.bindGroupLayout,
+                entries: [
+                    {
+                        binding: 0,
+                        resource: { buffer: uniforms },
+                    }
+                ]
+            }
+
+            this._bindGroup = this._device.createBindGroup(layoutDescriptor);
         }
 
-        return this._device.createBindGroup(layoutDescriptor);
+        return this._bindGroup;
     }
 }
