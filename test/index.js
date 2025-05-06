f3ca0cab8be8794c7c9c10e18196fd62bf290a3f
Ben Clarke <ben@arrayofstars.com>
2025-05-05 20:34:27 -0400

    Use ortho camera

--- test/index.js
+++ test/index.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   await renderer.init();
   
   const scene = renderer.createScene();
-  const camera = new tinygpu.PerspectiveCamera({ aspect: canvas.width / canvas.height, fov: Math.PI / 6 });
+  // const camera = new tinygpu.PerspectiveCamera({ aspect: canvas.width / canvas.height, fov: Math.PI / 6 });
+  const camera = new tinygpu.OrthographicCamera();
 
   const tex = new tinygpu.ImageTexture(
     "https://assets.codepen.io/1082534/141601-2560x1600-desktop-hd-ocean-wallpaper-image.jpg",
@@ -26,4 +27,13 @@ document.addEventListener("DOMContentLoaded", async () => {
   scene.add(mesh);
 
   renderer.render(scene, camera);
+
+  // let rot = 0;
+  // const animate = () => {
+  //   rot = performance.now() / 1000;
+  //   mesh.setRotation(0, rot, 0);
+  //   renderer.render(scene, camera);
+  //   requestAnimationFrame(animate);
+  // };
+  // animate();
 });
