8cb0ca8332e5aff8d3f63945026bfe119eb73b27
Ben Clarke <ben@arrayofstars.com>
2025-05-06 11:43:25 -0400

    Don't create a sampler per texture

--- src/texture.ts
+++ src/texture.ts
@@ -1,5 +1,5 @@
 export abstract class Texture {
-  constructor() {}
+  constructor() { }
 
   abstract get descriptor(): GPUTextureDescriptor;
   abstract get view(): GPUTextureView;
@@ -7,7 +7,6 @@ export abstract class Texture {
   abstract dispose(): void;
   abstract get width(): number;
   abstract get height(): number;
-  abstract get sampler(): GPUSampler;
 }
 
 export class DefaultTexture extends Texture {
@@ -15,7 +14,6 @@ export class DefaultTexture extends Texture {
 
   private _texture: GPUTexture | null = null;
   private _textureView: GPUTextureView | null = null;
-  private _sampler: GPUSampler | null = null;
   private _pixelData: Uint8Array = new Uint8Array([255, 255, 255, 255]);
 
   constructor() {
@@ -50,13 +48,6 @@ export class DefaultTexture extends Texture {
     return this._textureView;
   }
 
-  get sampler(): GPUSampler {
-    if (!this._sampler) {
-      throw new Error("Sampler not created");
-    }
-    return this._sampler;
-  }
-
   upload(device: GPUDevice): void {
     if (this._texture) {
       return;
@@ -69,11 +60,6 @@ export class DefaultTexture extends Texture {
       { offset: 0, bytesPerRow: 4, rowsPerImage: 1 },
       { width: 1, height: 1, depthOrArrayLayers: 1 },
     );
-
-    this._sampler = device.createSampler({
-      magFilter: "linear",
-      minFilter: "linear",
-    });
   }
 
   dispose(): void {
@@ -92,7 +78,6 @@ export class ImageTexture extends Texture {
   private _texture: GPUTexture | null = null;
   private _textureView: GPUTextureView | null = null;
   private _imagedata: ImageBitmap | null = null;
-  private _sampler: GPUSampler | null = null;
 
   constructor(imageSrc: string) {
     super();
@@ -147,13 +132,6 @@ export class ImageTexture extends Texture {
     return this._textureView;
   }
 
-  get sampler(): GPUSampler {
-    if (!this._sampler) {
-      throw new Error("Sampler not created");
-    }
-    return this._sampler;
-  }
-
   upload(device: GPUDevice): void {
     if (this._texture) {
       return;
@@ -169,11 +147,6 @@ export class ImageTexture extends Texture {
       { texture: this._texture, origin: { x: 0, y: 0 } },
       { width: this._width, height: this._height, depthOrArrayLayers: 1 },
     );
-
-    this._sampler = device.createSampler({
-      magFilter: "linear",
-      minFilter: "linear",
-    });
   }
 
   dispose(): void {
