aca3b95aa979468aa22f7c6923eceff7ff3451e7
Ben Clarke <ben@arrayofstars.com>
2025-05-07 11:16:51 -0400

    Move canvas resize out of scene, into renderer

--- src/scene.ts
+++ src/scene.ts
@@ -4,20 +4,10 @@ import { mat4, vec2, Vec2, vec3 } from "wgpu-matrix";
 import { UniformManager } from "./uniform-manager";
 
 export class Scene extends Transform {
-    private _device: GPUDevice;
-    private _resolution: Vec2;
-    private _canvas: HTMLCanvasElement;
-    private _observer: ResizeObserver;
     private _uniformManager: UniformManager;
 
-    constructor(device: GPUDevice, canvas: HTMLCanvasElement) {
+    constructor(device: GPUDevice) {
         super();
-
-        this._device = device;
-        this._canvas = canvas;
-
-        this._resolution = vec2.create();
-        this.canvasResize();
 
         this._uniformManager = new UniformManager(
             device,
@@ -25,26 +15,17 @@ export class Scene extends Transform {
                 { name: "projection matrix", value: mat4.create() },
                 { name: "view matrix", value: mat4.create() },
                 { name: "camera position", value: vec3.create() },
-                { name: "resolution", value: this._resolution },
+                { name: "resolution", value: vec2.create(1, 1) },
                 { name: "time", value: performance.now() / 1000 },
             ]
         );
     }
 
-    bindCanvasResize() {
-        this._observer = new ResizeObserver(this.canvasResize);
-        this._observer.observe(this._canvas);
-    }
-
-    canvasResize() {
-        this._resolution.set([this._canvas.offsetWidth, this._canvas.offsetHeight]);
-    }
-
-    update(camera: Camera) {
+    update(camera: Camera, resolution: Vec2) {
         this._uniformManager.updateUniform({ name: "projection matrix", value: camera.projectionMatrix });
         this._uniformManager.updateUniform({ name: "view matrix", value: camera.viewMatrix });
         this._uniformManager.updateUniform({ name: "camera position", value: camera.position });
-        this._uniformManager.updateUniform({ name: "resolution", value: this._resolution });
+        this._uniformManager.updateUniform({ name: "resolution", value: resolution });
         this._uniformManager.updateUniform({ name: "time", value: performance.now() / 1000 });
 
         this._uniformManager.update();
