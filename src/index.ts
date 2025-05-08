158d748d8c0aac2a69b907ca5a126f5ef98b63ae
Ben Clarke <ben@arrayofstars.com>
2025-05-07 21:53:51 -0400

    Modular exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 export { Renderer, RendererOptions } from "./renderer";
-export { BigTriangle } from "./geometry/big-triangle";
-export { CubeGeometry } from "./geometry/cube";
-export {
-  BasicMaterial,
-  BasicMaterialOptions,
-} from "./materials/basic-material";
 export { Color } from "./color";
 export { DefaultTexture, ImageTexture } from "./texture";
-export { PerspectiveCamera } from "./camera/perspective-camera";
-export { OrthographicCamera } from "./camera/orthographic-camera";
 export { Scene } from "./scene";
 export { Transform } from "./transform";
+
+export * as camera from "./camera";
+export * as geometry from "./geometry";
+export * as material from "./materials";
