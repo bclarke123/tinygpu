d8bf35817203b2d13159d5b8c0c37d89526736b9
Ben Clarke <ben@arrayofstars.com>
2025-05-15 07:29:51 -0400

    Expose lighting

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export * as geometry from "./geometry";
 export * as material from "./materials";
 export * as compute from "./compute/compute-task";
 export * as fluid from "./fluid";
+export * as lights from "./lights";
