42194636a253a78923b397bcb25502da988d308b
Ben Clarke <ben@arrayofstars.com>
2025-05-22 22:37:56 -0400

    Export videotexture

--- src/textures/index.ts
+++ src/textures/index.ts
@@ -4,3 +4,4 @@ export { ImageTexture } from "./image-texture";
 export { MappedTexture } from "./mapped-texture";
 export { Cubemap } from "./cubemap";
 export { DefaultCubemapTexture } from "./default-cubemap";
+export { VideoTexture } from "./video-texture";
