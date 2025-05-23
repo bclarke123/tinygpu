64973d82d77256daa098f9845da055b8dc66f9fa
Ben Clarke <ben@arrayofstars.com>
2025-05-22 22:37:29 -0400

    Add video texture

---
+++ src/textures/video-texture.ts
@@ -0,0 +1,31 @@
+export class VideoTexture {
+  private _texture: GPUExternalTexture;
+  private _video: HTMLVideoElement;
+  private _device: GPUDevice;
+
+  constructor(device: GPUDevice, video: HTMLVideoElement) {
+    this._device = device;
+    this._video = video;
+  }
+  get width(): number {
+    return this._video.width;
+  }
+  get height(): number {
+    return this._video.height;
+  }
+  get label(): string {
+    return this._texture?.label;
+  }
+  get format(): GPUTextureFormat {
+    return "rgba8unorm";
+  }
+  get dimension(): GPUTextureDimension {
+    return "2d";
+  }
+  get texture(): GPUExternalTexture {
+    this._texture = this._device.importExternalTexture({
+      source: this._video,
+    });
+    return this._texture;
+  }
+}
