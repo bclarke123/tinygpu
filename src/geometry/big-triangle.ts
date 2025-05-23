b7ba2b8e85ac0ec4bacb02bce8be832492d69be6
Ben Clarke <ben@arrayofstars.com>
2025-05-22 22:37:16 -0400

    Add empty tangents to big triangle

--- src/geometry/big-triangle.ts
+++ src/geometry/big-triangle.ts
@@ -5,9 +5,9 @@ export class BigTriangle extends Geometry {
     constructor(renderer: Renderer) {
         const vertexBuffer = renderer.createBuffer(
             new Float32Array([
-                -1, -1, 0, /* */ 0, 0, /* */ 0, 0, 1,
-                3, -1, 0, /* */ 2, 0, /* */ 0, 0, 1,
-                -1, 3, 0,  /* */ 0, 2, /* */ 0, 0, 1
+                -1, -1, 0, /* */ 0, 0, /* */ 0, 0, 1, 0, 0, 0,
+                3, -1, 0, /* */ 2, 0, /* */ 0, 0, 1, 0, 0, 0,
+                -1, 3, 0,  /* */ 0, 2, /* */ 0, 0, 1, 0, 0, 0
             ]),
             GPUBufferUsage.VERTEX
         );
@@ -28,23 +28,28 @@ export class BigTriangle extends Geometry {
         return [
             {
                 attributes: [
-                    {
+                    { // Position
                         shaderLocation: 0,
                         offset: 0,
                         format: "float32x3",
                     },
-                    {
+                    { // UV
                         shaderLocation: 1,
                         offset: 3 * 4,
                         format: "float32x2",
                     },
-                    {
+                    { // Normal
                         shaderLocation: 2,
                         offset: 5 * 4,
                         format: "float32x3",
+                    },
+                    { // Tangent
+                        shaderLocation: 3,
+                        offset: 8 * 4,
+                        format: "float32x3",
                     }
                 ],
-                arrayStride: 8 * 4,
+                arrayStride: 11 * 4,
                 stepMode: "vertex",
             }
         ]
